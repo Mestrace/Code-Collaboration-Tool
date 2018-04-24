8f7d76b57db15741eae4f22780f88ea15de76656
Yida <yida.liu@case.edu>
2018-04-24 17:38:25 -0400

    updated server code

--- src/multithread_server.py
+++ src/multithread_server.py
@@ -1,81 +1,97 @@
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
-from Crypto.Cipher import AES
-from Crypto import Random
-from random import randint
 from os import urandom
 from base64 import b64encode
+from encryption import *
 import rsa
 
-
+KEY_LENGTH = 1024
+BUFFER_SIZE = 1024
+SYSTEM_MESSAGE_PREFIX = "=="
+INVALID_NAME_MESSAGE = "The user name {0} is invalid, please try a new one"
+JOINED_CHAT_MESSAGE = "User {0} has joined the chat."
+LEFT_CHAT_MESSAGE = "User {0} has left the chat."
 
 class Client:
     '''
     a abstract class that includes all informations that server needs to connect with the client
     '''
-
-    def __init__(self, name, socket, address, pubkey):
+    def __init__(self, name, socket, address):
         self.name = name
         self.socket = socket
         self.address = address
-        self.aes_key = urandom(16)
-        self.aes_iv = urandom(16)
-        self.aes_cipher = AES.new(self.aes_key, AES.MODE_ECB, self.aes_iv)
-        self.SERVER_SOCKET_public_key, self.SERVER_SOCKET_private_key = rsa.newkeys(1024)
-        self.client_public_key = rsa.PublicKey.load_pkcs1(pubkey.encode("utf-8"))
-
-    def send_encrypting(self, msg):
-        '''
-        obtain an encrypted message by aes
-        '''
-        encrypted_msg = self.aes_cipher.encrypt(self.aespadding(msg).encode("utf-8"))
-        return b64encode(encrypted_msg).decode("utf-8")
-    
-    # Not implemented
-    def recv_decrypting(self, encrypted_msg):
-        '''
-        decrypted a received message
-        '''
-        return 0
-
-    @staticmethod
-    def aespadding(text, padding = ' '):
-        '''
-        add paddings to the end of text so that the length of the text is a multiply of 16
-        AES requires that the message to be encrypted can be divided into blocks
-        '''
-        l = len(text) % 16
-        n = int((16 - l)/len(padding))
-        if not l == 0:
-            return text + padding*n
-        else:
-            return text
 
 
-def welcome():
+def accept():
     '''
     server's welcome method
     '''
     while True:
-        client, client_address = SERVER_SOCKET.accept()
-        # TODO:
-        # Welcome message, contains the rsa key and encrypted aes
-        # pass the control to thread
+        sock, addr = SERVER_SOCKET.accept()
+        Thread(target=welcome, args=(sock, addr,)).start()
+
+
+def welcome(socket, address):
+    disconnect_cnt = 0
+    connect_cnt = 2
+    aeskey = urandom(16)
+    while connect_cnt != 0:
+        # string data
+        data = socket.recv(BUFFER_SIZE).decode()
+    
+        if connect_cnt == 2 and  data.startswith('-----BEGIN RSA PUBLIC KEY-----'):
+            #obtain public key from the user send encrypted aes key
+            pubkey = rsa.PublicKey.load_pkcs1(data.encode())
+            data = encrypt_rsa(aeskey, pubkey)
+            print("sending " + b64encode(data).decode("utf-8"))
+            socket.send(data)
+            connect_cnt -= 1
+        elif connect_cnt == 1:
+            name = decrypt_aes(aeskey, data)
+            # TODO: name validation
+            clients[socket] = Client(name, socket, address)
+            broadcast(JOINED_CHAT_MESSAGE.format(name), SYSTEM_MESSAGE_PREFIX)
+            connect_cnt -= 1
+        else:
+            # invalid connection without setting up phase
+            disconnect_cnt += 1
+            if disconnect_cnt > 20:
+                socket.send("Invalid connection.")
+                del clients[socket]
+                socket.close()
+                return
+    session(name, socket, aeskey)
+                
 
 # not implemented
-def session():
+def session(name, socket, aeskey):
     '''
     handles the communication with the client
     '''
-    return 0
+    while True:
+        msg = decrypt_aes(socket.recv(BUFFER_SIZE), aeskey)
+        if msg != bytes("{quit}", "utf-8"):
+             broadcast(msg, name + ": ")
+        else:
+            socket.send("Quitting")
+            socket.close()
+            del clients[socket]
+            broadcast(LEFT_CHAT_MESSAGE.format(name), SYSTEM_MESSAGE_PREFIX)
+            break
+    
+    
 
 # not implemented
-def broadcast():
+def broadcast(msg, prefix = ""):
     '''
     broadcast all messages to all the other clients
     '''
-    return 0
+    for c in clients:
+        c.socket.send(bytes(prefix, "utf-8") + msg)
     
+def is_valid_name(name):
+    return len(name) < 32
+
 # the dictionary indexed by sockets and stores a Client typed client informations
 clients = {}
 
@@ -84,3 +100,14 @@ PORT = 5000
 ADDR = (HOST, PORT)
 SERVER_SOCKET = socket(AF_INET, SOCK_STREAM)
 SERVER_SOCKET.bind(ADDR)
+
+if __name__ == "__main__":
+    SERVER_SOCKET.listen(5)
+    print("Waiting for connection...")
+    try:
+        ACCEPT_THREAD = Thread(target=accept)
+        ACCEPT_THREAD.start()
+    except KeyboardInterrupt:
+        ACCEPT_THREAD.join()
+        SERVER_SOCKET.close()
+
